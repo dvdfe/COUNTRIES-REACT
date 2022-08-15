38838e2a05913e0f276ce07e691fb4adc444a8b6
DVDFE <davidfe.pro@gmail.com>
2022-08-15 15:10:02 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo/>
+        <Navigation/>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
+        consequatur libero inventore minus earum, repellat alias recusandae ex!
+        Blanditiis vel earum at modi, aliquam eius voluptatem, aliquid veniam
+        asperiores amet cumque ex velit nesciunt, excepturi eligendi assumenda
+        quasi consectetur vero quis iste illo mollitia dolorem dignissimos.
+        Deleniti quibusdam cupiditate molestiae illum, sapiente culpa
+        accusantium aspernatur est quisquam? Quod aperiam, unde totam laboriosam
+        nesciunt, nisi, numquam tempora maxime commodi ut iure ea inventore esse
+        quo! Sit minus corrupti nesciunt error alias similique, sunt, vero et
+        atque dicta minima nemo optio fugit suscipit laudantium facere autem
+        explicabo, maxime excepturi consectetur enim quaerat?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero,
+        voluptas officiis vitae aperiam aliquam ea laboriosam dolorum? Similique
+        placeat aliquid unde ducimus fuga dolorum quasi repellendus et
+        asperiores sapiente itaque commodi eligendi consequuntur laboriosam
+        quidem odit ex, quas, doloremque officia expedita excepturi iste. Atque
+        esse ut dolores saepe pariatur delectus minima quasi! Nobis veniam,
+        provident voluptas culpa illum qui magnam magni quam! Neque ad inventore
+        officiis commodi autem molestiae iure nobis incidunt impedit. Itaque
+        minus sed deserunt rerum eos, pariatur hic beatae, maxime unde quas
+        nihil ipsam mollitia illum. Explicabo dolor, labore hic eos, dignissimos
+        incidunt atque a itaque exercitationem voluptates iure vel commodi
+        voluptatem laudantium porro numquam quam qui sapiente. Minus magni ea
+        commodi molestias eveniet, accusamus laboriosam quod amet reprehenderit,
+        blanditiis facilis cum voluptatem iste exercitationem. Numquam vitae
+        voluptatibus perferendis similique cum aut, debitis, velit laudantium
+        impedit expedita eos at excepturi, error autem corrupti est? Totam
+        labore quam at, amet voluptatem adipisci quia unde velit, eligendi vitae
+        sed earum numquam illo dolores minus repudiandae! Maiores enim quo,
+        saepe voluptas possimus aperiam iusto velit ea, excepturi odit
+        accusantium ex hic? Placeat fugiat libero molestias similique! Quia
+        voluptatibus explicabo eos veniam cupiditate quidem esse dolore
+        laboriosam doloremque est. Enim, voluptatibus?
+      </p>
+    </div>
+  );
+};
+
+export default About;
